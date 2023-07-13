1393ac5e4e08466a747a326383f401a2e95009c0
AbinayaTR <rsabinaya2002@gmail.com>
2023-07-13 12:49:20 +0530

    this is my second commit

--- D7-20230630/Homework/input.py
+++ D7-20230630/Homework/input.py
@@ -1,7 +1,8 @@
 name=input("Enter your name : ")
+print(f'Hii {name}')  
 age=int(input("Enter your age : "))
 dob=input("Enter your DOB : ")
 location=input("Enter your location : ")
 college_name=input("Enter your college name : ") 
 print("Hello, my name is ",name,". I am ",age,"years old and was born on ",dob,". Currently, I am located in ",location,"and i completed my degree at ",college_name)
-    
+ 
